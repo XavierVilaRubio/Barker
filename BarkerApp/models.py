97069a4d5e23542bdee0424db54af29d5b731142
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2022-03-09 19:32:46 +0100

    Started working on Models

--- BarkerApp/models.py
+++ BarkerApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+	bio = models.CharField(max_length=160)
+	avatar = models.TextField() # TODO: Take a look at ImageField
+
+	def __str__(self) -> str:
+		return self.user.username
+		
